c7030ff07e9d10c09b75494bae8b32f151b7cb30
Jala <raphael@jaladesign.com.au>
2023-08-22 00:38:54 +1000

    Added state change

--- src/app/_components/List.tsx
+++ src/app/_components/List.tsx
@@ -2,10 +2,19 @@
 import { useState } from "react"
 
 export default function List() {
+  const [change, setChange] = useState('')
+
+  const handleChange = (e: any) => {
+    console.log(e.target.value)
+    setChange(e.target.value)
+  }
+
   return(
     <div>
-
-      <input className="text-black w-full mt-2 px-2 py-1 rounded-sm border border-white block"/>
+      <ul>
+        <li className="text-slate-400">{change}</li>
+      </ul>
+      <input onChange={handleChange} className="text-black w-full mt-2 px-2 py-1 rounded-sm"/>
     </div>
   )
 }
